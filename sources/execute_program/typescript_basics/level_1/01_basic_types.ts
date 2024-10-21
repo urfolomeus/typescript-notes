d0df31bdb8d4e0265d22834b8da0762a0583c2ca
Alan Gardner <urfolomeus@gmail.com>
2024-10-21 08:19:13 +0100

    Fixing type hint

--- sources/execute_program/typescript_basics/level_1/01_basic_types.ts
+++ sources/execute_program/typescript_basics/level_1/01_basic_types.ts
@@ -1,7 +1,7 @@
 // We can specify the type of a variable with a type annotation.
-let sumNumber: number = 1 + 2;
+const sumNumber: number = 1 + 2;
 console.log(sumNumber);
 
 // If we assign a value of the wrong type to a variable, we'll get a type error.
-let sumString: number = 'any' + 'thing';
+const sumString: number = 'any' + 'thing';
 console.log(sumString);
